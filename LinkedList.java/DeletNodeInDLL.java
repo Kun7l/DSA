581fdac05bd872f2bccc8125769832a5925551fa
kun7l <kunalkhairanar007@gmail.com>
2025-01-23 10:53:09 +0530

    Deleting Node in DLL

---
+++ LinkedList.java/DeletNodeInDLL.java
@@ -0,0 +1,38 @@
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int val) {
+        data = val;
+        next = null;
+        prev = null;
+    }
+}
+public class DeletNodeInDLL {
+    public Node deleteNode(Node head, int x) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        if (x==1) {
+            head = head.next;
+            head.prev = null;
+            return head;
+        }
+        int count = 1;
+        Node temp = head;
+        while (count<x && temp!=null) {
+            temp = temp.next;
+            count++;
+        }
+        if(temp == null) return head;
+        if(temp.next == null){
+            temp.prev.next = null;
+        }
+        else{
+            temp.prev.next = temp.next;
+            temp.next.prev = temp.prev;
+        }
+        return head;
+    }
+}
