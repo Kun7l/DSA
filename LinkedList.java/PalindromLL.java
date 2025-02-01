5c87d9d20a3c035e0eab6bf936acffb59f33de48
kun7l <kunalkhairanar007@gmail.com>
2025-02-01 11:06:18 +0530

    solved palindrom LL optimal solution

--- LinkedList.java/PalindromLL.java
+++ LinkedList.java/PalindromLL.java
@@ -16,4 +16,45 @@ public class PalindromLL {
         }
         return true;
     }
+
+    public boolean optimalSol(ListNode head) {
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = reverseList(slow.next);
+        ListNode fist = head;
+        ListNode second = newHead;
+
+        while (second != null) {
+            if (fist.val != second.val) {
+                reverseList(newHead);
+                return false;
+            }
+            fist = fist.next;
+            second = second.next;
+        }
+        reverseList(newHead);
+        return true;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while (temp != null) {
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+
+    }
 }
