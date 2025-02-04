30181328374990f1afcb333d2a326bbf5f49729d
kun7l <kunalkhairanar007@gmail.com>
2025-02-04 10:02:42 +0530

    leetcode prob 2 more optimal solution

--- LinkedList.java/AddTwoNumbersUsingLL.java
+++ LinkedList.java/AddTwoNumbersUsingLL.java
@@ -44,4 +44,27 @@ public class AddTwoNumbersUsingLL {
         
         return ans.next;
     }
+    public ListNode optimal(ListNode l1, ListNode l2) {
+        ListNode ans = new ListNode(-1);
+        ListNode temp = ans;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2!=null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            ListNode dumNode = new ListNode(sum%10);
+            carry = (sum) / 10;
+            temp.next = dumNode;
+            temp = temp.next;
+
+        }
+        return ans.next;
+    }
 }
