e56b6d675bb205ad75a0c853b86f59d2d1992e11
kun7l <kunalkhairanar007@gmail.com>
2025-02-09 17:22:03 +0530

    solved leetcode prob : 1922

--- Recursion/CountGoodNumbers.java
+++ Recursion/CountGoodNumbers.java
@@ -1,17 +1,24 @@
 package Recursion;
 
 public class CountGoodNumbers {
+    long MOD = 1_000_000_007;
+
     public int countGoodNumbers(long n) {
-        int ans = 0;
-        if (n == 1) {
-            return 5;
-        } else {
-            if (n % 2 != 0) {
-                ans = ans * 4;
-            } else {
-                ans = ans * 5;
-            }
+        long even = (n+1)/2;
+        long odd = n/2;
+
+        return (int)((pow(5,even)*pow(4,odd))%MOD);
+    }
+
+    private long pow(long x, long n) {
+        if (n == 0) {
+            return 1;
         }
-        return ans * countGoodNumbers(n-1);
+        long temp = pow(x, n / 2);
+        if (n % 2 == 0) {
+            return (temp * temp) % MOD;
+        } else {
+            return (temp * temp * x) % MOD;
+        }
     }
 }
